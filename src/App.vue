4120164500414b816c8890bd135fdd6327bfc542
Hugo Barrera <exailer89@gmail.com>
2024-01-13 18:21:33 -0600

    Sigle File Components y ejemplo de como funcionan

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,19 @@
-<script setup>
+<!-- Las etiquetas <script>, <template> y <style> se les conoce como Single File Components (SFA)
+      * scripts: Logica de desarrollo.
+      * template: HTML que se mostrara en pantalla.
+      * style: CSS/Sass que tendra el componente.
+-->
 
+<script setup>
+  const hola = "Hola Mundo";
 </script>
 
 <template>
-  
+  <h1>{{ hola }}</h1>
 </template>
 
 <style scoped>
-
+  h1 {
+    text-transform: uppercase;
+  }
 </style>

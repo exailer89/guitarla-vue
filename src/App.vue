83e68b9e7015337d8c70d030956a95183cd0b1fa
Hugo Barrera <exailer89@gmail.com>
2024-02-18 11:43:27 -0600

    Almacenar Carrito en LocalStorage

--- src/App.vue
+++ src/App.vue
@@ -13,7 +13,16 @@
     onMounted(() => {
         guitarras.value = db;
         guitarra.value = db[3];
+
+        const carritoStorage = localStorage.getItem('carrito');
+        if(carritoStorage) {
+            carrito.value = JSON.parse(carritoStorage);
+        }
     });
+
+    const guardarLocalStorage = () => {
+        localStorage.setItem('carrito', JSON.stringify(carrito.value));
+    }
 
     const agregarCarrito = (guitarra) => {
         const existeCarrito = carrito.value.findIndex(producto => producto.id === guitarra.id);
@@ -24,26 +33,36 @@
             guitarra.cantidad = 1;
             carrito.value.push(guitarra);
         }
+
+        guardarLocalStorage();
     }
 
     const decrementarCantidad = (id) => {
         const index = carrito.value.findIndex(producto => producto.id === id);
         if(carrito.value[index].cantidad <= 1) return
         carrito.value[index].cantidad--
+
+        guardarLocalStorage();
     }
 
     const incrementarCantida = (id) => {
         const index = carrito.value.findIndex(producto => producto.id === id);
         if(carrito.value[index].cantidad >= 5) return
         carrito.value[index].cantidad++
+
+        guardarLocalStorage();
     }
 
     const eliminarProducto = (id) => {
         carrito.value = carrito.value.filter(producto => producto.id !== id);
+
+        guardarLocalStorage();
     }
 
     const vaciarCarrito = () => {
         carrito.value = [];
+
+        guardarLocalStorage();
     }
 </script>
 
